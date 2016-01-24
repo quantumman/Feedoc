ef4059f3952d87a8f0637f266d85ddaebd6db2b8
Shodai Yokoyama <quantumcars@gmail.com>
2016-01-24 21:40:00 +0900

    Add ListMenuItem

---
+++ src/components/ListMenuItem.jsx
@@ -0,0 +1,15 @@
+import '../style/MenuButton.scss';
+
+export default {
+  controller() {
+  },
+
+  view() {
+    return (
+      <button type="button" class="btn btn-menu">
+        <span class="glyphicon glyphicon-book glyphicon-large" aria-hidden="true">
+        </span>
+      </button>
+    );
+  },
+};
