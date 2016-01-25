5aa81db9d3f29672081d5bd40f88f10fe85be94b
Shodai Yokoyama <quantumcars@gmail.com>
2016-01-25 22:51:29 +0900

    Define routes

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.html?output=index.html';
 import App from './layouts/App.jsx';
 
 const routes = {
-  '/': app,
-  '/channels/:id': app,
-  '/channels/:id/posts': app,
-  '/channels/:id/posts/new': app,
-  '/channels/:id/posts/:id': app,
-  '/channels/:id/posts/:id/edit': app,
+  '/': App,
+  '/channels/:id': App,
+  '/channels/:id/posts': App,
+  '/channels/:id/posts/new': App,
+  '/channels/:id/posts/:id': App,
+  '/channels/:id/posts/:id/edit': App,
 };
 
 m.route(document.body, '/', routes);
