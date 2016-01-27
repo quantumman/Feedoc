b74cfe6dcc8da18a787210beb74baa3f6c8a0e28
Shodai Yokoyama <quantumcars@gmail.com>
2016-01-27 23:13:15 +0900

    Add Posts

---
+++ src/components/Posts.jsx
@@ -0,0 +1,53 @@
+import '../style/Posts.scss';
+
+import moment from 'moment';
+
+const vm = {
+  init() {
+  },
+};
+
+export default {
+  controller() {
+    vm.init();
+  },
+
+  view(_ctrl, args) {
+    return (
+      <div class="posts">
+        <ul class="list media-list">
+          {
+            args.posts.map(p => {
+              return (
+                <li key={p.id} class="item media">
+                  <div class="media-left">
+                    <a href="#">
+                      <img src={p.avatar} alt="avatar" />
+                    </a>
+                  </div>
+                  <div class="media-body">
+                    <h4 class="media-heading">
+                      <a
+                          href={`/channels/${args.channelId}/posts/${p.id}`}
+                          config={m.route}
+                      >
+                        {p.title}
+                      </a>
+                    </h4>
+                    <small>post date: {
+                      moment(
+                        p.createdOn,
+                        'dd MM D HH:mm:ss UTCZ YYYY'
+                      ).format('L')
+                    }
+                    </small>
+                  </div>
+                </li>
+              );
+            })
+          }
+        </ul>
+      </div>
+    );
+  },
+};
