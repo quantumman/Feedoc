920c89c9e78ddc720f4bf0e3512ef99625dea827
Shodai Yokoyama <quantumcars@gmail.com>
2016-01-27 23:14:38 +0900

    Modify routing

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import './style/index.scss';
 import './index.html?output=index.html';
 
 import App from './layouts/App.jsx';
+import MainPage from './pages/MainPage.jsx';
 
 const routes = {
   '/': App,
-  '/channels/:id': App,
-  '/channels/:id/posts': App,
+  '/channels': MainPage,
+  '/channels/:id': MainPage,
+  '/channels/:id/posts': MainPage,
   '/channels/:id/posts/new': App,
-  '/channels/:id/posts/:id': App,
-  '/channels/:id/posts/:id/edit': App,
+  '/channels/:id/posts/:postId': MainPage,
+  '/channels/:id/posts/:postId/edit': MainPage,
 };
 
 m.route(document.body, '/', routes);
