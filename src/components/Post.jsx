10b2ca08a0051a6fe55e060026de664708536001
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-05 02:39:18 +0900

    Post: Show live preview for markdown

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,5 +1,25 @@
+import marked from 'marked';
+marked.setOptions({
+  rendered: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: true,
+  pedantic: true,
+  sanitize: true,
+  smartLists: true,
+  smarttypants: true,
+});
+
 const vm = {
   init() {
+  },
+
+  config(markup) {
+    return (element, isInitialized) => {
+      if (!isInitialized && markup) {
+        $(element).html(marked(markup.content));
+      }
+    };
   },
 };
 
@@ -8,9 +28,11 @@ export default {
     vm.init();
   },
 
-  view() {
+  view(_ctrl, args) {
     return (
       <div class="post">
+        <div config={vm.config(args.post)}>
+        </div>
       </div>
     );
   },
