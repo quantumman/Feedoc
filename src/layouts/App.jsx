11a661c58e1da929a7df3f8158d05ae0fa4d747c
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-05 15:32:28 +0900

    MainPage: Remove left menu

--- src/layouts/App.jsx
+++ src/layouts/App.jsx
@@ -4,9 +4,6 @@ import 'js/bootstrap.js';
 
 import '../style/app.scss';
 
-import ListMenuItem from '../components/ListMenuItem.jsx';
-import VerticalMenu from './VerticalMenu.jsx';
-
 export default {
   controller() {
     m.redraw.strategy('diff');
@@ -17,12 +14,6 @@ export default {
       <div>
         <div class="fixed-menu-top">
           top
-        </div>
-        <div class="fixed-menu-left">
-          <VerticalMenu>
-            <div></div>
-            <ListMenuItem />
-          </VerticalMenu>
         </div>
         <div class="content">
           {children}
