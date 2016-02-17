25254e43f5efc1adb41ea9f25fd64b1c4b59ddb2
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-17 21:23:59 +0900

    TabMenu: Rewrite by polythene

--- src/layouts/TabMenu/index.jsx
+++ src/layouts/TabMenu/index.jsx
@@ -1,63 +1,61 @@
 import './style.scss';
+import PTabs from 'polythene/tabs/tabs';
 
 export const Tab = {
   controller(args) {
-    this.title = args.title;
-    this.isActive = args.isActive;
+    this.title = m.prop(args.title);
+    this.isActive = m.prop(args.isActive);
   },
 
   view(ctrl, args, children) {
+    ctrl.title(args.title);
+    ctrl.isActive(args.isActive);
     return (
-      <div>
-        {children}
-      </div>
+      <div>{children}</div>
+    );
+  },
+};
+
+const vm = {
+  init(_props, children) {
+    this.children = children
+       .map(c => c.controller());
+
+    this.buttons = this.children
+       .map((c, index) => {
+         return {
+           id: index,
+           label: c.title(),
+         };
+       });
+
+    this.selectedTab = m.prop(
+      this.children.find(c => c.isActive()).id
     );
   },
 };
 
 export const Tabs = {
-  controller() {
+  controller(props, children) {
+    vm.init(props, children);
   },
 
-  view(_ctrl, args, children) {
-    const tabs = children.map(c => c.controller());
+  view(_ctrl, args) {
     return (
       <div class="tab-menu">
         <div class="nav-menu">
           <h2 class="header">{args.feedName}</h2>
-          <ul class="nav nav-pills" role="tablist">
-            {
-              tabs.map((t, index) => {
-                return (
-                  <li role="presentation"
-                      class={t.isActive && 'active'}
-                  >
-                    <a href={`#${index}`}
-                       arial-controls={index}
-                       role="tab"
-                       data-toggle="pill"
-                    >
-                      {t.title}
-                    </a>
-                  </li>
-                );
-              })
-            }
-          </ul>
+          <PTabs buttons={vm.buttons}
+                autofit={true}
+                selectedTab={vm.selectedTab()}
+                activeSelected={true}
+                getState={state => {
+                  vm.selectedTab(state.index);
+                }}
+          />
         </div>
         <div class="tab-content">
-        {
-          tabs.map((t, index) => {
-            return (
-              <div role="tabpanel"
-                   class={t.isActive ? 'tab-pane active' : 'tab-pane'}
-                   id={index}
-              >
-                {t}
-              </div>
-            );
-          })
-        }
+          {vm.children[vm.selectedTab()]}
         </div>
       </div>
     );
