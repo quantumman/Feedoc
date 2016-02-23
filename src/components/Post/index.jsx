fe3cbbc1cd6be99ee79ef88a0bd5da5906e58356
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-23 23:37:47 +0900

    Post: Show toolbar

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -1,3 +1,8 @@
+import Toolbar from 'polythene/toolbar/toolbar';
+import IconArrowBack from 'mmsvg/google/msvg/navigation/arrow-back';
+import IconButton from 'polythene/icon-button/icon-button';
+import ModeEdit from 'mmsvg/google/msvg/editor/mode-edit';
+
 import marked from 'marked';
 marked.setOptions({
   rendered: new marked.Renderer(),
@@ -29,8 +34,17 @@ export default {
   },
 
   view(_ctrl, props) {
+    const toolbar = [
+      <IconButton icon={{ msvg: IconArrowBack }}
+                  events={{ onclick: () => window.history.back() }} />,
+      <span class="flex">
+        <h3 class="fixed-header-panel__title">{props.post().title}</h3>
+      </span>,
+      <IconButton icon={{ msvg: ModeEdit }} />,
+    ];
     return (
       <div class="post">
+        <Toolbar content={toolbar} />
         <div config={vm.config(props.post())}>
         </div>
       </div>
