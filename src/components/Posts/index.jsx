c0fa6930d1f8e722c458e12675ed385a7ab5317c
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-23 09:55:05 +0900

    Posts: Rewrite by polythene

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,6 +1,16 @@
 import './style.scss';
+import Button from 'polythene/button/button';
 import Icon from 'polythene/icon/icon';
 import book from 'mmsvg/google/msvg/action/book';
+
+const Avatar = {
+  view(_ctrl, props) {
+    return m(Icon, {
+      src: props.src,
+      class: 'pe-icon--avatar',
+    });
+  },
+};
 
 const vm = {
   init() {
@@ -26,11 +36,12 @@ export default {
                   <div class="timeline-content">
                     <div class="header">
                       <a href="#">
-                        <img src={p.avatar} alt="avatar" class="img-circle avatar" />
-                        <span class="username">{p.createdBy}</span>
+                        <Avatar src={p.creator.avatar} />
+                        <span class="username header-text">{p.createdBy}</span>
                       </a>
-                      <span class="content-type">
-                        <small><span class="tiny">POSTED</span> {p.contentType}</small>
+                      <span class="content-type header-text">
+                        <span class="tiny">POSTED </span>
+                        <small>{p.contentType}</small>
                       </span>
                     </div>
                     <div class="body">
@@ -50,7 +61,7 @@ export default {
                           p.contributors.slice(0, 6).map(i => {
                             return (
                               <span>
-                                <img src={i.avatar} alt="contributors" class="img-circle avatar" />
+                                <Avatar src={i.avatar} />
                               </span>
                             );
                           })
@@ -65,12 +76,8 @@ export default {
                         }
                       </div>
                       <div class="footer">
-                        <div class="btn-group" role="group" arial-label="footer">
-                          <button type="button" class="btn btn-default btn-sm">EDIT</button>
-                          <button type="button" class="btn btn-default btn-sm">
-                            COMMENTS ({p.comments})
-                          </button>
-                        </div>
+                        <Button label="EDIT" ink={false} />
+                        <Button label={`COMMENTS(${p.comments})`} ink={false} />
                       </div>
                     </div>
                   </div>
