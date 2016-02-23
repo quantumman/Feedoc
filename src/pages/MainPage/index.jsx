ebc59af17db0a8dac860787695ff047dd6d43437
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-23 22:07:58 +0900

    MainPage: Render Post

--- src/pages/MainPage/index.jsx
+++ src/pages/MainPage/index.jsx
@@ -3,6 +3,7 @@ import './style.scss';
 import App from '../../layouts/App';
 import { Tabs, Tab } from '../../layouts/TabMenu';
 import Feeds from '../../components/Feeds';
+import Post from '../../components/Post';
 import Posts from '../../components/Posts';
 
 export default {
@@ -17,17 +18,23 @@ export default {
             <div class="feeds-item">
               <Feeds {...props} />
             </div>
-            <div class="posts-item">
-              <Tabs feedName={props.feeds().find((f) => f.id === props.params.feedId).name}>
-                <Tab title="POST" isActive={false}>
-                </Tab>
-                <Tab title="TIMELINE" isActive={true}>
-                  <Posts {...props} />
-                </Tab>
-                <Tab title="WIP" isActive={false}>
-                </Tab>
-              </Tabs>
-            </div>
+            {
+              props.post
+              ? <Post {...props}></Post>
+              : (
+                  <div class="posts-item">
+                    <Tabs feedName={props.feeds().find((f) => f.id === props.params.feedId).name}>
+                      <Tab title="POST" isActive={false}>
+                      </Tab>
+                      <Tab title="TIMELINE" isActive={true}>
+                        <Posts {...props} />
+                      </Tab>
+                      <Tab title="WIP" isActive={false}>
+                      </Tab>
+                    </Tabs>
+                  </div>
+                )
+            }
           </div>
         </div>
       </App>
