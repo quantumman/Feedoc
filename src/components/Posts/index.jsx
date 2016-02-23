93fffb13da0f626e67caecce308f9f5797a82b41
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-23 21:38:20 +0900

    Post: Markup using Timeline

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,4 +1,5 @@
 import './style.scss';
+import { Timeline, Content } from '../../layouts/Timeline';
 import Button from 'polythene/button/button';
 import Icon from 'polythene/icon/icon';
 import book from 'mmsvg/google/msvg/action/book';
@@ -9,6 +10,73 @@ const Avatar = {
       src: props.src,
       class: 'pe-icon--avatar',
     });
+  },
+};
+
+const Header = {
+  view(_ctrl, props) {
+    return (
+      <div class="header">
+        <a href="#">
+          <Avatar src={props.creator.avatar} />
+          <span class="username header-text">{props.createdBy}</span>
+        </a>
+        <span class="content-type header-text">
+          <span class="tiny">POSTED </span>
+          <small>{props.contentType}</small>
+        </span>
+      </div>
+    );
+  },
+};
+
+const Body = {
+  view(_ctrl, props) {
+    return (
+      <div class="body">
+        <h3>
+          <a
+              href={`/feeds/${props.feedId}/posts/${props.id}`}
+              config={m.route}
+          >
+            {props.title}
+          </a>
+        </h3>
+        <div class="contributors">
+          <small>
+            CONTRIBUTORS
+          </small>
+          {
+            props.contributors.slice(0, 6).map(i => {
+              return (
+                <span>
+                  <Avatar src={i.avatar} />
+                </span>
+              );
+            })
+          }
+          {
+            props.contributors.length >= 6
+            ? (
+              <span class="vertical-middle glyphicon glyphicon-option-horizontal">
+              </span>
+            )
+              : ''
+          }
+        </div>
+      </div>
+    );
+  },
+};
+
+const Footer = {
+  view(_ctrl, props) {
+    return (
+      <div class="footer">
+        <Button label="EDIT" ink={false} />
+        <Button label={`COMMENTS(${props.comments})`} ink={false} />
+      </div>
+    );
   },
 };
 
@@ -25,67 +93,23 @@ export default {
   view(_ctrl, args) {
     return (
       <div class="posts">
-        <ul class="list">
-          {
-            args.posts().map(p => {
-              return (
-                <li key={p.id} class="item">
-                  <div class="timeline-icon">
-                    <Icon msvg={book} />
-                  </div>
-                  <div class="timeline-content">
-                    <div class="header">
-                      <a href="#">
-                        <Avatar src={p.creator.avatar} />
-                        <span class="username header-text">{p.createdBy}</span>
-                      </a>
-                      <span class="content-type header-text">
-                        <span class="tiny">POSTED </span>
-                        <small>{p.contentType}</small>
-                      </span>
-                    </div>
-                    <div class="body">
-                      <h3>
-                        <a
-                            href={`/feeds/${args.params.feedId}/posts/${p.id}`}
-                            config={m.route}
-                        >
-                          {p.title}
-                        </a>
-                      </h3>
-                      <div class="contributors">
-                        <small>
-                          CONTRIBUTORS
-                        </small>
-                        {
-                          p.contributors.slice(0, 6).map(i => {
-                            return (
-                              <span>
-                                <Avatar src={i.avatar} />
-                              </span>
-                            );
-                          })
-                        }
-                        {
-                          p.contributors.length >= 6
-                          ? (
-                            <span class="vertical-middle glyphicon glyphicon-option-horizontal">
-                            </span>
-                          )
-                          : ''
-                        }
-                      </div>
-                      <div class="footer">
-                        <Button label="EDIT" ink={false} />
-                        <Button label={`COMMENTS(${p.comments})`} ink={false} />
-                      </div>
-                    </div>
-                  </div>
-                </li>
-              );
-            })
-          }
-        </ul>
+        <Timeline>
+        {
+          args.posts().map(p => {
+            const header = <Header {...p} />;
+            const body = <Body {...p} feedId={args.params.feedId} />;
+            const footer = <Footer {...p} />;
+            return (
+              <Content
+                  icon={book}
+                  header={header}
+                  body={body}
+                  footer={footer}
+              />
+            );
+          })
+        }
+        </Timeline>
       </div>
     );
   },
