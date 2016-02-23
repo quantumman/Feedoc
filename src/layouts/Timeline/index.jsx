684098cb71b0cb4f2f5fae380702027873dbc3b6
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-23 10:13:36 +0900

    Timeline: Markup Content

--- src/layouts/Timeline/index.jsx
+++ src/layouts/Timeline/index.jsx
@@ -2,9 +2,24 @@ import './style.scss';
 import Icon from 'polythene/icon/icon';
 
 export const Content = {
-  view() {
+  controller(props) {
+    this.icon = props.icon;
+  },
+
+  view(_ctrl, props) {
     return (
       <div class="timeline-content">
+        <div class="timeline-header">
+          {props.header}
+        </div>
+        <div class="timeline-box">
+          <div class="timeline-body">
+            {props.body}
+          </div>
+          <div class="timeline-footer">
+            {props.footer}
+          </div>
+        </div>
       </div>
     );
   },
