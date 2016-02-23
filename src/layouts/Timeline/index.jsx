a544063915c730e6ee5f49212cb8d299f8172921
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-23 09:59:27 +0900

    layouts: Add Timeline

---
+++ src/layouts/Timeline/index.jsx
@@ -0,0 +1,20 @@
+import './style.scss';
+import Icon from 'polythene/icon/icon';
+
+export const Content = {
+  view() {
+    return (
+      <div class="timeline-content">
+      </div>
+    );
+  },
+};
+
+export const Timeline = {
+  view() {
+    return (
+      <div class="timeline">
+      </div>
+    );
+  },
+};
