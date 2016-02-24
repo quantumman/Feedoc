8ab0ff191880a43d49d626f4f2a182219237bd8d
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-24 09:52:42 +0900

    layouts/MainPage: Markup MainPage

--- src/layouts/MainPage/index.jsx
+++ src/layouts/MainPage/index.jsx
@@ -1,4 +1,7 @@
 import './style.scss';
+import u from '../../utility';
+
+import App from '../App';
 
 export const LeftPanel = {
   controller() {
@@ -25,9 +28,30 @@ export const Content = {
 };
 
 export const MainPage = {
-  view(_ctrl, _props) {
+  view(_ctrl, _props, children) {
+    const leftPanel = u.single(
+      children,
+      c => c.controller().name === 'MainPage.LeftPanel'
+    );
+    const content = u.single(
+      children,
+      c => c.controller().name === 'MainPage.Content'
+    );
+
     return (
       <div class="main-page">
+        <App>
+          <div class="wrap">
+            <div class="container">
+              <div class="left-panel">
+                {leftPanel}
+              </div>
+              <div class="content">
+                {content}
+              </div>
+            </div>
+          </div>
+        </App>
       </div>
     );
   },
