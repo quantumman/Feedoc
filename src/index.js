38dbd0bb04e2403572d92616e176cfae1045bde5
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-24 10:09:08 +0900

    pages: Rename Home

--- src/index.js
+++ src/index.js
@@ -3,16 +3,16 @@ import './index.html?output=index.html';
 
 import App from './layouts/App';
 import container from './Container.jsx';
-import MainPage from './pages/MainPage';
+import Home from './pages/Home';
 
 const routes = {
   '/': App,
-  '/feeds': container(MainPage),
-  '/feeds/:id': container(MainPage),
-  '/feeds/:id/posts': container(MainPage),
+  '/feeds': container(Home),
+  '/feeds/:id': container(Home),
+  '/feeds/:id/posts': container(Home),
   '/feeds/:id/posts/new': App,
-  '/feeds/:id/posts/:postId': container(MainPage),
-  '/feeds/:id/posts/:postId/edit': container(MainPage),
+  '/feeds/:id/posts/:postId': container(Home),
+  '/feeds/:id/posts/:postId/edit': container(Home),
 };
 
 m.route(document.body, '/', routes);
