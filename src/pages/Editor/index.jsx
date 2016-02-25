79d98defa412bd070b1ff99262acf6db71ac9f98
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-26 08:27:28 +0900

    Editor: Show MarkdownEditor

--- src/pages/Editor/index.jsx
+++ src/pages/Editor/index.jsx
@@ -1,9 +1,15 @@
 import './style.scss';
 
+import App from '../../layouts/App';
+import MarkdownEditor from '../../components/MarkdownEditor';
+
 export default {
-  view(_ctrl, _props, _children) {
+  view(_ctrl, props, _children) {
     return (
       <div class="edit">
+        <App>
+          <MarkdownEditor src={props.markdown} />
+        </App>
       </div>
     );
   },
