7c5860bde4d1043cfe87b82c0815de9738fd5cc8
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-26 08:27:14 +0900

    Define a route to Editor page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.html?output=index.html';
 import App from './layouts/App';
 import container from './Container.jsx';
 import { FeedsPage, PostsPage, PostViewPage } from './pages/Home';
+import Editor from './pages/Editor';
 
 const routes = {
   '/': App,
@@ -12,7 +13,7 @@ const routes = {
   '/feeds/:id/posts': container(PostsPage),
   '/feeds/:id/posts/new': App,
   '/feeds/:id/posts/:postId': container(PostViewPage),
-  '/feeds/:id/posts/:postId/edit': container(PostViewPage),
+  '/feeds/:id/posts/:postId/edit': container(Editor),
 };
 
 m.route(document.body, '/', routes);
