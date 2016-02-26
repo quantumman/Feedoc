0c56ad915420b29f78d6dcee0ac1a6c34c2ca395
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-26 09:33:30 +0900

    Markdown: Add a hook to render markdown

--- src/components/Markdown/index.jsx
+++ src/components/Markdown/index.jsx
@@ -1,5 +1,25 @@
 import './style.scss';
 
+import marked from 'marked';
+marked.setOptions({
+  rendered: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: true,
+  pedantic: true,
+  sanitize: true,
+  smartLists: true,
+  smarttypants: true,
+});
+
+function config(markdown) {
+  return (element, isInitialized) => {
+    if (!isInitialized && markdown) {
+      $(element).html(marked(markdown));
+    }
+  };
+}
+
 export default {
   view() {
     return (
