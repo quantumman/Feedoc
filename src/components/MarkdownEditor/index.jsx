c66dec7d0d5265e78f54525f831b1d3f94a2e6f8
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-26 09:40:14 +0900

    MarkdownEditor: Add preview area

--- src/components/MarkdownEditor/index.jsx
+++ src/components/MarkdownEditor/index.jsx
@@ -3,6 +3,7 @@ import './style.scss';
 import Toolbar from 'polythene/toolbar/toolbar';
 import IconArrowBack from 'mmsvg/google/msvg/navigation/arrow-back';
 import IconButton from 'polythene/icon-button/icon-button';
+import Markdown from '../Markdown';
 
 const toolbar = [
   <IconButton icon={{ msvg: IconArrowBack }}
@@ -10,13 +11,15 @@ const toolbar = [
 ];
 
 export default {
-  view(_ctrl, _props) {
+  view(_ctrl, props) {
     return (
       <div class="markdown-editor">
         <Toolbar content={toolbar} />
         <div class="editor">
           <div class="edit-area">TEST</div>
-          <div class="preview-area">TEST</div>
+          <div class="preview-area">
+            <Markdown src={props.src()}/>
+          </div>
         </div>
       </div>
     );
