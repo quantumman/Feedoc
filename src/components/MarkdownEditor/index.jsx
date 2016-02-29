e8c941aab8209e193d6420d440b17d9dd14e4103
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-29 10:02:07 +0900

    MarkdownEditor: Show toolbar

--- src/components/MarkdownEditor/index.jsx
+++ src/components/MarkdownEditor/index.jsx
@@ -1,9 +1,15 @@
 import './style.scss';
 
+import Toolbar from 'polythene/toolbar/toolbar';
+
+const toolbar = [
+];
+
 export default {
   view(_ctrl, _props) {
     return (
       <div class="markdown-editor">
+        <Toolbar content={toolbar} />
       </div>
     );
   },
