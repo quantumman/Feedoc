4c1be6577993429049258d554632992df164312f
Shodai Yokoyama <quantumcars@gmail.com>
2016-02-29 10:02:45 +0900

    MarkdownEditor: Show a button to go back previous page

--- src/components/MarkdownEditor/index.jsx
+++ src/components/MarkdownEditor/index.jsx
@@ -1,8 +1,12 @@
 import './style.scss';
 
 import Toolbar from 'polythene/toolbar/toolbar';
+import IconArrowBack from 'mmsvg/google/msvg/navigation/arrow-back';
+import IconButton from 'polythene/icon-button/icon-button';
 
 const toolbar = [
+  <IconButton icon={{ msvg: IconArrowBack }}
+              events={{ onclick: () => window.history.back() }} />,
 ];
 
 export default {
