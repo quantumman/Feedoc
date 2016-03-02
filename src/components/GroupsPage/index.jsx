78bd9b5152ca637f85a8459caee2da99d7167251
Shodai Yokoyama <quantumcars@gmail.com>
2016-03-03 08:59:15 +0900

    components: Add GroupsPage

---
+++ src/components/GroupsPage/index.jsx
@@ -0,0 +1,37 @@
+import './style.scss';
+
+import { Root, LeftView, MiddleView, RightView } from '../Root';
+import GroupList from '../Groups';
+import PostList from '../Posts';
+import Markdown from '../Markdown';
+
+import Groups from '../../models/groups';
+import Posts from '../../models/posts';
+
+export default {
+  controller() {
+    const teamId = this.teamId = m.route.param('teamId');
+    const groupId = this.groupId = m.route.param('groupId');
+    const postId = this.postId = m.route.param('postId');
+    this.groups = this.groups || Groups.retrieve({ teamId });
+    this.posts = this.posts || Posts.retrieve({ teamId, groupId });
+    this.post = this.post || Posts.get({ teamId, groupId, postId });
+    this.src = this.post && this.post.then(p => p.content);
+  },
+
+  view(ctrl) {
+    return (
+      <Root>
+        <LeftView>
+          <GroupList {...ctrl} />
+        </LeftView>
+        <MiddleView>
+          <PostList {...ctrl} />
+        </MiddleView>
+        <RightView>
+          <Markdown {...ctrl} />
+        </RightView>
+      </Root>
+    );
+  },
+};
