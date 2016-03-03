07b2a38c722eb6161c076c8a39c46a9603986c49
Shodai Yokoyama <quantumcars@gmail.com>
2016-03-03 10:04:43 +0900

    index.js: Add a route to GroupsPage

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './index.html?output=index.html';
 import 'polythene/theme/theme';
 import 'polythene/layout/theme/theme';
 
+import GroupsPage from './components/GroupsPage';
+
 const noop = {};
 
 const routes = {
   '/:teamId': noop,
   '/:teamId/new': noop,
-  '/:teamId/groups': noop,
+  '/:teamId/groups': GroupsPage,
+  '/:teamId/groups/:groupId/posts': GroupsPage,
+  '/:teamId/groups/:groupId/posts/:postId': GroupsPage,
   '/:teamId/tags': noop,
   '/:teamId/settings': noop,
 };
