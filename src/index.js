ffa108be206f1876e6638dabb08df07230e6cc30
Shodai Yokoyama <quantumcars@gmail.com>
2016-03-03 09:02:42 +0900

    index.js: Remove old routes

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import './style/index.scss';
 import './index.html?output=index.html';
 
-import App from './layouts/App';
-import container from './Container.jsx';
-import { FeedsPage, PostsPage, PostViewPage } from './pages/Home';
-import Editor from './pages/Editor';
+import 'polythene/theme/theme';
+import 'polythene/layout/theme/theme';
+
+const noop = {};
 
 const routes = {
-  '/': App,
-  '/feeds': container(FeedsPage),
-  '/feeds/:id': container(PostsPage),
-  '/feeds/:id/posts': container(PostsPage),
-  '/feeds/:id/posts/new': App,
-  '/feeds/:id/posts/:postId': container(PostViewPage),
-  '/feeds/:id/posts/:postId/edit': container(Editor),
+  '/:teamId': noop,
+  '/:teamId/new': noop,
+  '/:teamId/groups': noop,
+  '/:teamId/tags': noop,
+  '/:teamId/settings': noop,
 };
 
 m.route(document.body, '/', routes);
