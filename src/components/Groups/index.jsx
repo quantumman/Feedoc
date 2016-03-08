e3da3448a015fef400e2df97e6e561c56593d4c0
Shodai Yokoyama <quantumcars@gmail.com>
2016-03-08 19:16:51 +0900

    Groups: Show list of groups

--- src/components/Groups/index.jsx
+++ src/components/Groups/index.jsx
@@ -1,7 +1,27 @@
+import './style.scss';
+
+import List from 'polythene/list/list';
+import ListTile from 'polythene/list-tile/list-tile';
+
 export default {
-  view() {
+  view(_ctrl, props) {
+    const groups = props.groups || m.prop([]);
+
+    const tiles = groups().map(g => {
+      return (
+        <ListTile title={g.name}
+                  positionSelected={false}
+                  ink={false}
+                  url={{ href: `/${props.teamId}/groups/${g.id}/posts`, config: m.route }}
+        />
+      );
+    });
+
     return (
       <div class="groups">
+        <List hoverable={true}
+              tiles={tiles}
+        />
       </div>
     );
   },
