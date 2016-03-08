b499d4b6dfe8e7da5a0a7ea79b348422030343e9
Shodai Yokoyama <quantumcars@gmail.com>
2016-03-08 20:01:47 +0900

    Root: Determine layouts

--- src/components/Root/index.jsx
+++ src/components/Root/index.jsx
@@ -1,10 +1,36 @@
 import './style.scss';
 
-export default {
+import App from '../App';
+
+const Empty = {
   view() {
+    return <div></div>;
+  },
+};
+
+export default {
+  controller(initialProps) {
+    return initialProps;
+  },
+
+  view(ctrl) {
+    const Groups = ctrl.GroupList || Empty;
+    const Posts = ctrl.PostList || Empty;
+    const Markdown = ctrl.Markdown || Empty;
     return (
       <App>
         <div class="root">
+          <div class="groups-page layout">
+            <div class="- list-view left">
+              <Groups {...ctrl} />
+            </div>
+            <div class="- list-view middle">
+              <Posts {...ctrl} />
+            </div>
+            <div class="flex right">
+              <Markdown {...ctrl} />
+            </div>
+          </div>
         </div>
       </App>
     );
