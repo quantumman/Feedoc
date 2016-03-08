dae422e3fd964376e8c12569b2d1914e59f806f7
Shodai Yokoyama <quantumcars@gmail.com>
2016-03-08 19:35:55 +0900

    Posts: Show list of posts

--- src/components/Posts/index.jsx
+++ src/components/Posts/index.jsx
@@ -1,9 +1,31 @@
 import './style.scss';
 
+import List from 'polythene/list/list';
+import ListTile from 'polythene/list-tile/list-tile';
+
 export default {
-  view() {
+  view(_ctrl, props) {
+    const posts = props.posts || m.prop([]);
+
+    const tiles = posts().map(p => {
+      return (
+        <ListTile title={p.title}
+                  highSubtitle={<div>{p.creator.name}</div>}
+                  positionSelected={false}
+                  ink={true}
+                  url={{
+                    href: `/${props.teamId}/groups/${p.groupId}/posts/${p.id}`,
+                    config: m.route,
+                  }}
+        />
+      );
+    });
+
     return (
       <div class="posts">
+        <List hoverable={true}
+              tiles={tiles}
+        />
       </div>
     );
   },
