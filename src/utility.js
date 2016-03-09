ce1eccb1d806f73f6b9d6fcec6e5027383da0c5e
Shodai Yokoyama <quantumcars@gmail.com>
2016-03-10 08:43:05 +0900

    utility: Add singleOrDefault

--- src/utility.js
+++ src/utility.js
@@ -1,10 +1,21 @@
-export default {
+const u = {
   single: (array, pred) => {
+    const x = u.singleOrDefault(array, pred);
+    if (!x) {
+      throw new Error('One or more elements are found');
+    }
+
+    return x;
+  },
+
+  singleOrDefault: (array, pred) => {
     const xs = array.filter(pred);
     if (xs.length !== 1) {
-      throw new Error('One or more elements are found');
+      return undefined;
     }
 
     return xs[0];
   },
 };
+
+export default u;
