32b7812b638814be06b050458c25bcff263722e4
Shodai Yokoyama <quantumcars@gmail.com>
2016-03-10 22:59:22 +0900

    app.js: Load frontend app

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -13,6 +13,8 @@
 // to also remove its path from "config.paths.watched".
 import 'phoenix_html';
 
+import './index.js';
+
 // Import local files
 //
 // Local files can be imported directly using relative
