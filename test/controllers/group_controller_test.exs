0af0b5bf74e469c13e57933bc57833635e549cad
Shodai Yokoyama <quantumcars@gmail.com>
2016-03-16 22:51:20 +0900

    group_controller: Add tests

--- test/controllers/group_controller_test.exs
+++ test/controllers/group_controller_test.exs
@@ -1,60 +1,65 @@
 defmodule Feedoc.GroupControllerTest do
   use Feedoc.ConnCase
 
+  alias Feedoc.TestDataFactory, as: Factory
   alias Feedoc.Group
   @valid_attrs %{name: "some content"}
-  @invalid_attrs %{}
+  @invalid_attrs %{name: nil}
 
   setup %{conn: conn} do
-    {:ok, conn: put_req_header(conn, "accept", "application/json")}
+    team = Factory.create(:team)
+    {:ok, conn: put_req_header(conn, "accept", "application/json"), team: team}
   end
 
-  test "lists all entries on index", %{conn: conn} do
-    conn = get conn, group_path(conn, :index)
-    assert json_response(conn, 200)["data"] == []
+  test "lists all entries on index", %{conn: conn, team: team} do
+    conn = get conn, team_group_path(conn, :index, team.id)
+    assert json_response(conn, 200) == []
   end
 
-  test "shows chosen resource", %{conn: conn} do
-    group = Repo.insert! %Group{}
-    conn = get conn, group_path(conn, :show, group)
-    assert json_response(conn, 200)["data"] == %{"id" => group.id,
+  test "shows chosen resource", %{conn: conn, team: team} do
+    group = Factory.create(:group, %{team_id: team.id})
+    conn = get conn, team_group_path(conn, :show, team.id, group)
+    assert json_response(conn, 200) == %{
+      "id" => group.id,
       "name" => group.name,
-      "team_id" => group.team_id}
+      "team_id" => group.team_id
+    }
   end
 
-  test "does not show resource and instead throw error when id is nonexistent", %{conn: conn} do
+  test "does not show resource and instead throw error when id is nonexistent", %{conn: conn, team: team} do
     assert_error_sent 404, fn ->
-      get conn, group_path(conn, :show, -1)
+      get conn, team_group_path(conn, :show, team.id, -1)
     end
   end
 
-  test "creates and renders resource when data is valid", %{conn: conn} do
-    conn = post conn, group_path(conn, :create), group: @valid_attrs
-    assert json_response(conn, 201)["data"]["id"]
+  test "creates and renders resource when data is valid", %{conn: conn, team: team} do
+    group = Factory.create(:group, %{team_id: team.id})
+    conn = post conn, team_group_path(conn, :create, team.id), @valid_attrs
+    assert json_response(conn, 201)["id"]
     assert Repo.get_by(Group, @valid_attrs)
   end
 
-  test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, group_path(conn, :create), group: @invalid_attrs
+  test "does not create resource and renders errors when data is invalid", %{conn: conn, team: team} do
+    conn = post conn, team_group_path(conn, :create, team.id), @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
-  test "updates and renders chosen resource when data is valid", %{conn: conn} do
-    group = Repo.insert! %Group{}
-    conn = put conn, group_path(conn, :update, group), group: @valid_attrs
-    assert json_response(conn, 200)["data"]["id"]
+  test "updates and renders chosen resource when data is valid", %{conn: conn, team: team} do
+    group = Factory.create(:group, %{team_id: team.id})
+    conn = put conn, team_group_path(conn, :update, team.id, group), @valid_attrs
+    assert json_response(conn, 200)["id"]
     assert Repo.get_by(Group, @valid_attrs)
   end
 
-  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    group = Repo.insert! %Group{}
-    conn = put conn, group_path(conn, :update, group), group: @invalid_attrs
+  test "does not update chosen resource and renders errors when data is invalid", %{conn: conn, team: team} do
+    group = Factory.create(:group, %{team_id: team.id})
+    conn = put conn, team_group_path(conn, :update, team.id, group), @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
-  test "deletes chosen resource", %{conn: conn} do
-    group = Repo.insert! %Group{}
-    conn = delete conn, group_path(conn, :delete, group)
+  test "deletes chosen resource", %{conn: conn, team: team} do
+    group = Factory.create(:group, %{team_id: team.id})
+    conn = delete conn, team_group_path(conn, :delete, team.id, group)
     assert response(conn, 204)
     refute Repo.get(Group, group.id)
   end
