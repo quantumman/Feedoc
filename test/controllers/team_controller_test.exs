7833b2182d65dcc70eabca409d39451dcc9f03a0
Shodai Yokoyama <quantumcars@gmail.com>
2016-03-16 22:52:52 +0900

    team_controller: Add tests

--- test/controllers/team_controller_test.exs
+++ test/controllers/team_controller_test.exs
@@ -1,9 +1,10 @@
 defmodule Feedoc.TeamControllerTest do
   use Feedoc.ConnCase
 
+  alias Feedoc.TestDataFactory, as: Factory
   alias Feedoc.Team
   @valid_attrs %{name: "some content"}
-  @invalid_attrs %{}
+  @invalid_attrs %{name: nil}
 
   setup %{conn: conn} do
     {:ok, conn: put_req_header(conn, "accept", "application/json")}
@@ -11,14 +12,16 @@ defmodule Feedoc.TeamControllerTest do
 
   test "lists all entries on index", %{conn: conn} do
     conn = get conn, team_path(conn, :index)
-    assert json_response(conn, 200)["data"] == []
+    assert json_response(conn, 200) == []
   end
 
   test "shows chosen resource", %{conn: conn} do
-    team = Repo.insert! %Team{}
+    team = Factory.create(:team)
     conn = get conn, team_path(conn, :show, team)
-    assert json_response(conn, 200)["data"] == %{"id" => team.id,
-      "name" => team.name}
+    assert json_response(conn, 200) == %{
+      "id" => team.id,
+      "name" => team.name
+    }
   end
 
   test "does not show resource and instead throw error when id is nonexistent", %{conn: conn} do
@@ -28,31 +31,31 @@ defmodule Feedoc.TeamControllerTest do
   end
 
   test "creates and renders resource when data is valid", %{conn: conn} do
-    conn = post conn, team_path(conn, :create), team: @valid_attrs
-    assert json_response(conn, 201)["data"]["id"]
+    conn = post conn, team_path(conn, :create), @valid_attrs
+    assert json_response(conn, 201)["id"]
     assert Repo.get_by(Team, @valid_attrs)
   end
 
   test "does not create resource and renders errors when data is invalid", %{conn: conn} do
-    conn = post conn, team_path(conn, :create), team: @invalid_attrs
+    conn = post conn, team_path(conn, :create), @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
   test "updates and renders chosen resource when data is valid", %{conn: conn} do
-    team = Repo.insert! %Team{}
-    conn = put conn, team_path(conn, :update, team), team: @valid_attrs
-    assert json_response(conn, 200)["data"]["id"]
+    team = Factory.create(:team)
+    conn = put conn, team_path(conn, :update, team), @valid_attrs
+    assert json_response(conn, 200)["name"] == @valid_attrs[:name]
     assert Repo.get_by(Team, @valid_attrs)
   end
 
   test "does not update chosen resource and renders errors when data is invalid", %{conn: conn} do
-    team = Repo.insert! %Team{}
-    conn = put conn, team_path(conn, :update, team), team: @invalid_attrs
+    team = Factory.create(:team)
+    conn = put conn, team_path(conn, :update, team), @invalid_attrs
     assert json_response(conn, 422)["errors"] != %{}
   end
 
   test "deletes chosen resource", %{conn: conn} do
-    team = Repo.insert! %Team{}
+    team = Factory.create(:team)
     conn = delete conn, team_path(conn, :delete, team)
     assert response(conn, 204)
     refute Repo.get(Team, team.id)
