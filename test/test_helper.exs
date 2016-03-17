44a581c63724838e3866167bc0e7d6c2d87530bc
Shodai Yokoyama <quantumcars@gmail.com>
2016-03-17 21:33:41 +0900

    test_helper: Start Faker

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,7 @@
 {:ok, _} = Application.ensure_all_started(:ex_machina)
 
 ExUnit.start
+Faker.start
 
 Mix.Task.run "ecto.create", ~w(-r Feedoc.Repo --quiet)
 Mix.Task.run "ecto.migrate", ~w(-r Feedoc.Repo --quiet)
