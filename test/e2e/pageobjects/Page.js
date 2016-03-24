566f2f2068c18e63618cb0df72bfeffdc9c81d11
Shodai Yokoyama <quantumcars@gmail.com>
2016-03-24 21:23:31 +0900

    Page.js: Add _go

--- test/e2e/pageobjects/Page.js
+++ test/e2e/pageobjects/Page.js
@@ -1,4 +1,12 @@
+function _go(id) {
+  return browser.click('#' + id);
+}
+
 export default class Page {
+  constructor() {
+    _go.bind(this);
+  }
+
   open(path) {
     browser.url('/?/' + path);
   }
