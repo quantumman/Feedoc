3a882bb3a80e6262e9ee889636289bed38da06f2
Shodai Yokoyama <quantumcars@gmail.com>
2016-03-24 23:03:45 +0900

    Groups.page.js: Add groups

--- test/e2e/pageobjects/Groups.page.js
+++ test/e2e/pageobjects/Groups.page.js
@@ -5,6 +5,18 @@ class GroupsPage extends Page {
     super.open('e2e');
     super.groups();
   }
+
+  get groups() {
+    return browser
+      .elements('.groups .pe-list-tile')
+      .value
+      .map(e => {
+        return {
+          title: browser.elementIdElement(e.ELEMENT, '.pe-list-tile__title').getText(),
+          link: browser.elementIdElement(e.ELEMENT, 'a.pe-list-tile__primary'),
+        };
+      });
+  }
 }
 
 export default new GroupsPage();
