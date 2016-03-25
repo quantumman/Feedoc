d097a22e1b9ee58ee7de159800a2082559653f99
Shodai Yokoyama <quantumcars@gmail.com>
2016-03-25 09:18:02 +0900

    lib: Add Data.Factory module

---
+++ lib/data/factory.ex
@@ -0,0 +1,26 @@
+defmodule Data.Factory do
+  use ExMachina.Ecto, repo: Feedoc.Repo
+
+  alias Feedoc.Team
+  alias Feedoc.Group
+  alias Feedoc.Post
+
+  def factory(:team) do
+    %Team{
+      name: Faker.Internet.domain_suffix(),
+    }
+  end
+
+  def factory(:group) do
+    %Group{
+      name: Faker.Lorem.Shakespeare.En.hamlet()
+    }
+  end
+
+  def factory(:post) do
+    %Post{
+      title: Faker.Commerce.En.product_name(),
+      content: "#Markdown"
+    }
+  end
+end
